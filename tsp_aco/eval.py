4ede940d29d481b8516d6b401cb5107c70f60f05
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2025-12-08 21:09:40 +0700

    aa

--- tsp_aco/eval.py
+++ tsp_aco/eval.py
@@ -5,6 +5,14 @@ from scipy.spatial import distance_matrix
 from concurrent.futures import ThreadPoolExecutor
 from aco import run_tsp_aco
 
+OPTIMAL = {
+    "50": 5.706510615938985,
+    "100": 7.7894944810761135, 
+    "200": 10.678417491907155,
+    "300": 13.002009389982788,
+    "500": 16.547266843098683
+}
+
 def eval_instance(coords, n_ants, n_iter, seed):
     D = distance_matrix(coords, coords)
     return run_tsp_aco(D, n_ants, n_iter, seed)
@@ -27,9 +35,12 @@ def main(size):
     print(f"Processing TSP instances of size {size}...")
     current_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(current_dir, 'datasets', f'test_TSP{size}.npy')
-    list_costs = process_file(path, n_ants=50, n_iter=100)
-    mean_cost = np.mean(list_costs, axis=0) # Average costs over all instances
-    print(mean_cost.tolist())
+    list_costs = process_file(path, n_ants=50, n_iter=200)
+    mean_cost = np.mean(list_costs, axis=0).tolist() # Average costs over all instances
+    best = mean_cost[-1]
+    optimal = OPTIMAL[str(size)]
+    opt_gap = (best - optimal) / optimal * 100
+    print(f"Optimal gap for TSP{size}: {opt_gap:.2f}%")
 
 if __name__ == "__main__":
     size = int(sys.argv[1]) if len(sys.argv) > 1 else 100
